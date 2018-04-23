6ded2583e557ba196c46fc8d667c3fe63d7f95db
Eli Sandine <elisandine@10-248-177-213.wireless.oregonstate.edu>
2018-04-23 13:53:13 -0700

    Added to tests to AppTest.java

--- projects/sandinee/Assignment-2/Calendar/src/test/java/calendar/ApptTest.java
+++ projects/sandinee/Assignment-2/Calendar/src/test/java/calendar/ApptTest.java
@@ -18,10 +18,71 @@ public class ApptTest  {
       assertEquals(0, appt0.getRecurIncrement());
       appt0.setValid();
   }
-@Test(timeout = 4000)
- public void test01()  throws Throwable  {
+  @Test(timeout = 4000)
+  public void test01()  throws Throwable  {
+	  	// Set the private methods of the class
+	  	int startHour = 22;
+        int startMinute = 30;
+        int startDay = 12;
+        // Month is out of range
+        int startMonth = 15;
+        int startYear = 2018;
+        String title = "Dentist Appt";
+        String description = "Have to get implent replaced";
+        String emailAddress = "xxx@yahoo.com";
 
+        // Use constructor to build the new Appt()...
+        Appt appt1 = new Appt(
+                      startHour, startMinute, startDay, startMonth,
+                      startYear, title, description, emailAddress
+                      );
+        appt1.toString();
 
-}
+        // assertTrue that this appt is valid
+        assertTrue(appt1.getValid() );
+        // assert that all values from above are equal to the new appt1 we set
+        assertEquals(22, appt1.getStartHour() );
+        assertEquals(30, appt1.getStartMinute() );
+        assertEquals(12, appt1.getStartDay() );
+        assertEquals(15, appt1.getStartMonth() );
+        assertEquals(2018, appt1.getStartYear() );
+        assertEquals("Dentist Appt", appt1.getTitle() );
+        assertEquals("Have to get implent replaced", appt1.getDescription() );
+        assertEquals("xxx@yahoo.com", appt1.getEmailAddress() );
+
+  }
+  @Test(timeout = 4000)
+  public void test02() throws Throwable {
+	  	// Set the private methods of the class
+	  	int startHour = 12;
+	  	int startMinute = 35;
+	  	int startDay = 40;
+	  	// Month is out of range
+	  	int startMonth = 10;
+	  	int startYear = 2018;
+	  	String title = "Doctor Appt";
+	  	String description = "Have to get arm fixed";
+	  	String emailAddress = "yyy@yahoo.com";
+
+	  	// Use constructor to build the new Appt()...
+	  	Appt appt2 = new Appt(
+                    startHour, startMinute, startDay, startMonth,
+                    startYear, title, description, emailAddress
+                    );
+	  	appt2.toString();
+
+	  	// assertTrue that this appt is valid
+	  	assertTrue(appt2.getValid() );
+	  	// assert that all values from above are equal to the new appt1 we set
+	  	assertEquals(12, appt2.getStartHour() );
+	  	assertEquals(35, appt2.getStartMinute() );
+	  	assertEquals(40, appt2.getStartDay() );
+	  	assertEquals(10, appt2.getStartMonth() );
+	  	assertEquals(2018, appt2.getStartYear() );
+	  	assertEquals("Doctor Appt", appt2.getTitle() );
+	  	assertEquals("Have to get arm fixed", appt2.getDescription() );
+	  	assertEquals("yyy@yahoo.com", appt2.getEmailAddress() );
+
+  }
 
 }
